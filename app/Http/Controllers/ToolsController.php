71ae2d98771eecc225074117262b9e1ee0e9170f
cy322666 <cy322666@gmail.com>
2023-08-30 10:09:52 +0200

    add active

---
+++ app/Http/Controllers/ToolsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Services\amoCRM\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ToolsController extends Controller
+{
+    public function active(Request $request)
+    {
+        Log::info(__METHOD__, $request->toArray());
+
+        $leadId = $request->toArray()['leads']['add'][0]['id'];
+
+        $amoApi = (new Client(Account::query()->first()))
+            ->init()
+            ->initLogs();
+
+        $leadBase = $amoApi
+            ->service
+            ->leads()
+            ->find($leadId);
+
+        $leads = $leadBase->contact->leads;
+
+        if ($leads->count() > 1) {
+            $leadsActive = $leads->filter(function ($lead) {
+                if ($lead->status_id != 142 && $lead->status_id != 143) {
+                    return $lead;
+                }
+            });
+        }
+
+        if (!empty($leadsActive) && $leadsActive->count() > 1) {
+
+            $leadBase->attachTag('В работе');
+        }
+    }
+}
