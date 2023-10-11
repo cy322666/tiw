e1159455665ca9f9700ad2fb8b9c6802cb700a98
cy322666 <cy322666@gmail.com>
2023-10-11 12:58:14 +0200

    tg link update

--- app/Http/Controllers/TgController.php
+++ app/Http/Controllers/TgController.php
@@ -48,7 +48,7 @@ class TgController extends Controller
             $transaction->save();
         }
 
-        return \redirect(Env::get('TG_CHAT_LINK').'?start='.$request->lead_id);
+        return \redirect(\env('TG_CHAT_LINK').'?start='.$request->lead_id);
     }
 
     //своя реализация кажется не актуальна
